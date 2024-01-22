d43dcf92295b9fc56055a50b7cab516a1ebabaab
Michal Trubini <mchl.trubini@gmail.com>
2024-01-22 15:26:47 +0100

    wip

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -42,7 +42,7 @@ class Book extends Model
                 $q->whereBetween('created_at', [$from, $to]);
             }
         
-        }])->orderBy('reviews_count', 'desc')->limit(10);
+        }])->orderBy('reviews_count', 'desc');
     }
 
 
@@ -56,5 +56,33 @@ class Book extends Model
     {
 
         return $query->withAvg('reviews', 'rating')->orderBy('reviews_avg_rating', 'desc');
+    }
+
+    public function scopePopularLastMonth(Builder $query): Builder
+    {
+        return $query->popular(now()->subMonth(), now())
+            ->highestRated(now()->subMonth(), now())
+            ->minReviews(2);
+    }
+
+    public function scopePopularLast6Months(Builder $query): Builder
+    {
+        return $query->popular(now()->subMonths(6), now())
+            ->highestRated(now()->subMonths(6), now())
+            ->minReviews(5);
+    }
+
+    public function scopeHighestRatedLastMonth(Builder $query): Builder
+    {
+        return $query->highestRated(now()->subMonth(), now())
+            ->popular(now()->subMonth(), now())
+            ->minReviews(2);
+    }
+
+    public function scopeHighestRatedLast6Months(Builder $query): Builder
+    {
+        return $query->highestRated(now()->subMonths(6), now())
+            ->popular(now()->subMonths(6), now())
+            ->minReviews(5);
     }
 }
--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -2,8 +2,28 @@
 
 @section('content')
     <h1 class="mb-10 text-2xl">Books</h1>
-    <form>
+    <form method="GET" action="{{route('books.index')}}" class="mb-4 flex items-center space-x-2">
+        <input type="text" name="title" class="input h-10" placeholder="Search by title" value="{{request('title')}}">
+        <input type="hidden" name="filter" value="{{request('filter')}}">
+        <button type="submit" class="btn">Search</button>
+        <a href="{{route('books.index')}}" class="btn h-10">Clear</a>
     </form>
+
+    <div class="filter-container mb-4 flex">
+        @php
+            $filters = [
+                '' => 'Latest',
+                'popular_last_month' => 'Popular last month',
+                'popular_last_6months' => 'Popular last 6 months',
+                'highest_rated_last_month' => 'Highest rated last month',
+                'highest_rated_last_6months' => 'Highest rated last 6 months',
+            ];
+        @endphp
+
+        @foreach ($filters as $filter => $label)
+            <a href="{{route('books.index', [...request()->query(), 'filter' => $filter])}}" class="{{request('filter', '') === $filter ? 'filter-item-active' : 'filter-item'}}">{{$label}}</a>
+        @endforeach
+    </div>
     <ul>
         @forelse ($books as $book)
         <li class="mb-4">
